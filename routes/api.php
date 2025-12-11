8c2b2beb9caf5cfc636dfb7e7fc679ceea5d1e80
mediaclockdev <sb@mediaclock.com.au>
2025-12-11 15:28:30 +1100

    changes

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Auth\OtpPasswordController;
+
 
 Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'signup']);
     Route::post('login', [AuthController::class, 'login']);
-   // Route::get('/me', [AuthController::class, 'me']);
+    Route::post('password/otp/request', [OtpPasswordController::class, 'requestOtp']);
+    Route::post('password/otp/reset', [OtpPasswordController::class, 'resetWithOtp']);
+    // Route::get('/me', [AuthController::class, 'me']);
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('logout', [AuthController::class, 'logout']);
